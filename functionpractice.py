4e887e8fa21e3eef5daf24f0dbe9916731e7976e
Casey Pastella <casey.pastella.15@cnu.edu>
2019-05-04 16:34:14 -0400

    update

--- functionpractice.py
+++ functionpractice.py
@@ -1,13 +1,41 @@
-def find33(list1):
+#Challenge problems
+
+def spygame(intlist):
     
-    for num in range(0,len(list1) -1): 
-        if list1[num] == 3 and list1[num +1] == 3: 
-            return True
-        else:
-            return False
-    
-find33([1,3,3])
+    code = [0,0,7,"x"]
+    for num in intlist:
+        if num == code[0]:
+            code.pop(0)
+        
+    return len(code) == 1
+
+spygame([1,0,3,2,0,7])
 
 
+#countprimes
+
+def count_primes(num):
+    if num == 0 or num == 1:
+        return 0 
+
+    primes = [2] 
+    x = 3 
+    while x <= num:
+        for y in primes: 
+            if x%y == 0:
+                x+= 2
+                break 
+            
+        else: 
+            primes.append(x)
+            x+=2
+    print(primes)
+    return len(primes)
+
+count_primes(100)
+
+
+            
+    
 
     
